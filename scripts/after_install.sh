fb5eb63132d73f82f2f18107482a798d68c91475
sahiba shahab <92646376+sahibashahab@users.noreply.github.com>
2024-12-05 23:31:24 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ec2-user/server
+# Log the output of all commands to /tmp/after_install.log
+exec > >(tee /tmp/after_install.log) 2>&1
+set -x  # Enable command tracing for debugging
 
-#_Remove_Unused_Code
+# Change to the server directory
+cd /home/ec2-user/server
+echo "Changed to /home/ec2-user/server"
+
+# Remove unused code (node_modules and build)
+echo "Removing unused code..."
 rm -rf node_modules
 rm -rf build
 
-#Install_node_modules_&_Make_React_Build
-npm  install
+# Install node_modules and make the React build
+echo "Installing node modules..."
+npm install --verbose
+
+echo "Running npm build..."
 npm run build
